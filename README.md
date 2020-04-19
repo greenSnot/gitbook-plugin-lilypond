75ca18ff625fcafc7b8dae31487e02c7eb5bbfdf
t <email@example.com>
2020-04-19 09:25:16 -0400

    feat: error reporter

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 ### gitbook-plugin-lilypond
 
+### Requirements
+
+* lilypond cli
+
+### Example
+
+```
+\`\`\`lilypond
+  \new Staff <<
+    {c''2}
+    \\
+    {e'2}
+    \\
+    {c'8}
+    \\
+    {d'8}
+    \\
+    {<e'>8}
+    \\
+    {b''8}
+    \\
+    {g'8}
+    \\
+    {f'8}
+    \\
+    {a'''8}
+  >>
+\`\`\`
+```
--- index.js
+++ index.js
@@ -11,7 +11,16 @@ module.exports = {
         for (var i = 0, len = umls.length; i < len; i++) {
           const tmpfile = path.join(tmpdir, Math.random().toString(16).substr(2));
           fs.writeFileSync(tmpfile, umls[i].replace(/```lilypond/i, '').replace(/```\s*$/i, ''));
-          child_process.execSync(`cd ${tmpdir} && lilypond -dbackend=svg ${tmpfile}`);
+          try {
+            child_process.execSync(`cd ${tmpdir} && lilypond -dbackend=svg ${tmpfile}`);
+          } catch (e) {
+            page.content = page.content.replace(
+              umls[i],
+              '```\n' + e.message + '```\n',
+            );
+            fs.unlinkSync(tmpfile);
+            continue;
+          }
           const tmpsvg = tmpfile + '.svg';
           const lilypond_svg = fs.readFileSync(tmpsvg).toString();
           page.content = page.content.replace(
